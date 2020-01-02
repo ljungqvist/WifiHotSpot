e59da381682075507b322af6fc424b709fdf2580
Petter Ljungqvist <petter.ljungqvist@houston-inc.com>
2020-01-03 00:07:32 +0200

    updated stuff

--- app/src/main/java/com/fitc/wifihotspot/MagicActivity.java
+++ app/src/main/java/com/fitc/wifihotspot/MagicActivity.java
@@ -9,20 +9,22 @@ import android.widget.Toast;
 
 public class MagicActivity extends PermissionsActivity {
 
-    public static void useMagicActivityToTurnOn(Context c){
-        Uri uri = new Uri.Builder().scheme(c.getString(R.string.intent_data_scheme)).authority(c.getString(R.string.intent_data_host_turnon)).build();
-        Toast.makeText(c,"Turn on. Uri: "+uri.toString(),Toast.LENGTH_LONG).show();
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(uri);
-        c.startActivity(i);
+    private static Intent intent(Uri uri) {
+        return new Intent(Intent.ACTION_VIEW)
+                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                .setData(uri);
     }
 
-    public static void useMagicActivityToTurnOff(Context c){
+    public static void useMagicActivityToTurnOn(Context c) {
+        Uri uri = new Uri.Builder().scheme(c.getString(R.string.intent_data_scheme)).authority(c.getString(R.string.intent_data_host_turnon)).build();
+        Toast.makeText(c, "Turn on. Uri: " + uri.toString(), Toast.LENGTH_LONG).show();
+        c.startActivity(intent(uri));
+    }
+
+    public static void useMagicActivityToTurnOff(Context c) {
         Uri uri = new Uri.Builder().scheme(c.getString(R.string.intent_data_scheme)).authority(c.getString(R.string.intent_data_host_turnoff)).build();
-        Toast.makeText(c,"Turn off. Uri: "+uri.toString(),Toast.LENGTH_LONG).show();
-        Intent i = new Intent(Intent.ACTION_VIEW);
-        i.setData(uri);
-        c.startActivity(i);
+        Toast.makeText(c, "Turn off. Uri: " + uri.toString(), Toast.LENGTH_LONG).show();
+        c.startActivity(intent(uri));
     }
 
     private static final String TAG = MagicActivity.class.getSimpleName();
@@ -50,8 +52,6 @@ public class MagicActivity extends PermissionsActivity {
         HotSpotIntentService.start(this, intent);
         finish();
     }
-
-
 
 
 }
